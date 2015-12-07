78c82f0e58d621f5f4d6688267c070d3347d80fc
gonzasestopal <gonzasestopal@gmail.com>
2015-12-06 21:21:09 -0600

    layout

--- krreame/static/js/krreame.js
+++ krreame/static/js/krreame.js
@@ -1,27 +1,13 @@
-$(document).ready(function()
-{
-  $('#load-more-content').click(function()
-  {
-    
-    $('#more-content').toggleClass('shown');
+$(document).ready(function() {
 
-    $('#load-more-content').hide();
-
-    if( $('#more-content').hasClass('shown') )
-    {
-      $('#load-more-content').text('Hide content');
-      $('#more-content').fadeIn('slow', function()
-      {
-        $('#load-more-content').fadeIn('slow');
-      });
-    }
-    else
-    {
-      $('#load-more-content').text('Load some content');
-      $('#more-content').fadeOut('slow', function()
-      {
-        $('#load-more-content').fadeIn('slow');
-      });
-    }
+  // Initialize Masonry
+  $('#content').masonry({
+    columnWidth: 320,
+    itemSelector: '.item',
+    isFitWidth: true,
+    isAnimated: !Modernizr.csstransitions
+  }).imagesLoaded(function() {
+    $(this).masonry('reload');
   });
+
 });
